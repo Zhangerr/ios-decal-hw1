c535d12469d2ff980818d27d8b3e9c6f51ed8dad
Alex Zhang <alex.zhang@berkeley.edu>
2015-09-15 02:34:22 -0700

    finish hw1

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -5,49 +5,50 @@
 
 import UIKit
 
-
 //: ## Q1: Optionals
 //: The compiler is yelling at us. Why isn't this the correct way to unwrap optionals?
+var str  :String = "hello";
 
 class Foo {
     
     var wordA : String!
     var wordB : String!
     
+    
+    
+    
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]
+        wordB = words[1]
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q1 HERE]
-    
-
+//: The optionals actually don't need to be unwrapped, since it's fine to assign an optional to implicitly unwrapped optionals, which `wordA` and `wordB` are. Having a question mark in the original context just didn't make sense, since that implies optional chaining which is unnecessary given there is no method call right after either assignments and the return value is already of type `String?`. If `wordA` and `wordB` weren't defined as optionals, then you would just append `!` to the end of both lines of assignment.
     
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
+    static func arePalindromes(words: [String]) -> Bool! {
         let reversedWords = words.map() {String($0.characters.reverse())}
         var numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q2 HERE]
+//: The compiler dislikes the for loop because `i` originally was defined with a `let` statement, which means its value cannot be modified. Subsequently, the loop counter cannot increment and the loop will not function properly. `true` should be returned instead of `nil`
     
     
     
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
+    class func isAnagram(wordA: String, wordB: String) -> Bool? {
+        var countLetters : [Character : Int] = [Character:Int]()
         var lenA = wordA.characters.count
         var lenB = wordB.characters.count
         
@@ -81,12 +82,11 @@ class Foo {
             }
         }
         
-        return nil
+        return true
     }
 }
 
-//: [EXPLAIN YOUR ANSWER TO Q3 HERE]
-
+//: `countLetters` was not initialized so it was causing errors when the function tried using it. Also, `true` should be returned instead of `nil`
 
 //: **Do not** change anything below.
 //: You should be able to call the methods as is.
